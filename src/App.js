da7425a9d751454c482a0330d149d858deccd826
Graycemuthui <graycemuthui20@gmail.com>
2022-07-06 19:24:59 +0300

    Style heading

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import './index.css';
-import Home from './components/Home';
-import Nav from './components/Nav';
-import Quote from './components/Quote';
+import Home from './components/Navlinks/Home';
+import Nav from './components/Navlinks/Nav';
+import Quote from './components/Navlinks/Quote';
 
 // eslint-disable-next-line react/prefer-stateless-function
 const App = () => (
@@ -17,7 +17,6 @@ const App = () => (
         <Route path="/calculator" element={<Calculator />} />
       </Routes>
     </Router>
-    <h2 className="heading-text">Lets do Math!</h2>
   </div>
 );
 
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -19,101 +19,104 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
-      <p className="result" onChange={handleTextChange}>
-        {state.next || state.total || 0}
-      </p>
-      <div>
-        <button type="button" onClick={handleClick} value="AC">
-          AC
-        </button>
-        <button type="button" onClick={handleClick} value="+/-">
-          +/-
-        </button>
-        <button type="button" onClick={handleClick} value="%">
-          %
-        </button>
-        <button
-          className="operator"
-          type="button"
-          onClick={handleClick}
-          value="÷"
-        >
-          /
-        </button>
-      </div>
-      <div>
-        <button type="button" onClick={handleClick} value="7">
-          7
-        </button>
-        <button type="button" onClick={handleClick} value="8">
-          8
-        </button>
-        <button type="button" onClick={handleClick} value="9">
-          9
-        </button>
-        <button
-          className="operator"
-          type="button"
-          onClick={handleClick}
-          value="x"
-        >
-          x
-        </button>
-      </div>
-      <div>
-        <button type="button" onClick={handleClick} value="4">
-          4
-        </button>
-        <button type="button" onClick={handleClick} value="5">
-          5
-        </button>
-        <button type="button" onClick={handleClick} value="6">
-          6
-        </button>
-        <button
-          className="operator"
-          type="button"
-          onClick={handleClick}
-          value="-"
-        >
-          -
-        </button>
-      </div>
-      <div>
-        <button type="button" onClick={handleClick} value="1">
-          1
-        </button>
-        <button type="button" onClick={handleClick} value="2">
-          2
-        </button>
-        <button type="button" onClick={handleClick} value="3">
-          3
-        </button>
-        <button
-          className="operator"
-          type="button"
-          onClick={handleClick}
-          value="+"
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <button id="zero-btn" type="button" onClick={handleClick} value="0">
-          0
-        </button>
-        <button type="button" onClick={handleClick} value=".">
-          .
-        </button>
-        <button
-          className="operator"
-          type="button"
-          onClick={handleClick}
-          value="="
-        >
-          =
-        </button>
+    <div className="calculator-app">
+      <h2 className="heading-text">Lets do Math!</h2>
+      <div className="calculator">
+        <p className="result" onChange={handleTextChange}>
+          {state.next || state.total || 0}
+        </p>
+        <div>
+          <button type="button" onClick={handleClick} value="AC">
+            AC
+          </button>
+          <button type="button" onClick={handleClick} value="+/-">
+            +/-
+          </button>
+          <button type="button" onClick={handleClick} value="%">
+            %
+          </button>
+          <button
+            className="operator"
+            type="button"
+            onClick={handleClick}
+            value="÷"
+          >
+            /
+          </button>
+        </div>
+        <div>
+          <button type="button" onClick={handleClick} value="7">
+            7
+          </button>
+          <button type="button" onClick={handleClick} value="8">
+            8
+          </button>
+          <button type="button" onClick={handleClick} value="9">
+            9
+          </button>
+          <button
+            className="operator"
+            type="button"
+            onClick={handleClick}
+            value="x"
+          >
+            x
+          </button>
+        </div>
+        <div>
+          <button type="button" onClick={handleClick} value="4">
+            4
+          </button>
+          <button type="button" onClick={handleClick} value="5">
+            5
+          </button>
+          <button type="button" onClick={handleClick} value="6">
+            6
+          </button>
+          <button
+            className="operator"
+            type="button"
+            onClick={handleClick}
+            value="-"
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <button type="button" onClick={handleClick} value="1">
+            1
+          </button>
+          <button type="button" onClick={handleClick} value="2">
+            2
+          </button>
+          <button type="button" onClick={handleClick} value="3">
+            3
+          </button>
+          <button
+            className="operator"
+            type="button"
+            onClick={handleClick}
+            value="+"
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button id="zero-btn" type="button" onClick={handleClick} value="0">
+            0
+          </button>
+          <button type="button" onClick={handleClick} value=".">
+            .
+          </button>
+          <button
+            className="operator"
+            type="button"
+            onClick={handleClick}
+            value="="
+          >
+            =
+          </button>
+        </div>
       </div>
     </div>
   );
